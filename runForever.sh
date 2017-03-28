93015824496e82c1863c2d51ac16deff7150bd25
Joseph <joebean13@outlook.com>
2017-03-28 20:09:50 +0100

    Update (see Issue #4)

--- runForever.sh
+++ runForever.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # If you have issues running this try this code
-# sudo chmod 777 runForever.sh
+# sudo chmod +x runForever.sh
 echo If you get any issues running this script then read the help in the file
 while :
 do
