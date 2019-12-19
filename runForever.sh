6f587194a0e855f74cecbf38968ae6b33c2584bb
k3in0x <43535917+k3in0x@users.noreply.github.com>
2019-12-19 13:44:12 +0100

    Delete runForever.sh

--- runForever.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# If you have issues running this try this code
-# sudo chmod +x runForever.sh
-echo If you get any issues running this script then read the help in the file
-while :
-do
-	node bot.js
-done
